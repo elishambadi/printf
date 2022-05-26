bf83075160d3821cad74732df9bb609ba0a73937
elishambadi <embadi43@gmail.com>
2022-05-26 01:36:55 +0000

    more betty fixes

--- print_nums.c
+++ print_nums.c
@@ -9,17 +9,17 @@
  */
 int print_int(va_list l, flags_t *f)
 {
-    int n = va_arg(l, int);
-    int res = count_digit(n);
+	int n = va_arg(l, int);
+	int res = count_digit(n);
 
-    if (f->space == 1 && f->plus == 0 && n >= 0)
-        res += _putchar(' ');
-    if (f->plus == 1 && n >= 0)
-        res += _putchar('+');
-    if (n <= 0)
-        res++;
-    print_number(n);
-    return (res);
+	if (f->space == 1 && f->plus == 0 && n >= 0)
+		res += _putchar(' ');
+	if (f->plus == 1 && n >= 0)
+		res += _putchar('+');
+	if (n <= 0)
+		res++;
+	print_number(n);
+	return (res);
 }
 
 /**
@@ -31,11 +31,11 @@ int print_int(va_list l, flags_t *f)
  */
 int print_unsigned(va_list l, flags_t *f)
 {
-    unsigned int u = va_arg(l, unsigned int);
-    char *str = convert(u, 10, 0);
+	unsigned int u = va_arg(l, unsigned int);
+	char *str = convert(u, 10, 0);
 
-    (void)f;
-    return (_puts(str));
+	(void)f;
+	return (_puts(str));
 }
 
 /**
